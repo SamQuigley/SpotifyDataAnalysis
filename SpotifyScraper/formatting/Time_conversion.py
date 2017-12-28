6eaf0401201586a7f89f10ac1eb240ba83a4ad23
samquigley <samq2011@live.com>
2017-12-28 02:50:07 +0000

    Working on inserting new rows and columns

--- SpotifyScraper/formatting/Time_conversion.py
+++ SpotifyScraper/formatting/Time_conversion.py
@@ -4,13 +4,6 @@ with open(path) as csvfile:
     readCSV = csv.reader(csvfile, delimiter='\t')
     count=0
     for row in readCSV:
-        # ms=int(row[16])
-        # ms = int(ms)
-        # secs=(ms/1000)%60
-        # secs=int(secs)
-        # mins=(ms/(1000*60))%60
-        # mins=int(mins)
-        # print ("%d:%d" % (mins, secs))
         try:
             ms=int(row[16])
             ms = int(ms)
@@ -18,10 +11,12 @@ with open(path) as csvfile:
             secs=int(secs)
             mins=(ms/(1000*60))%60
             mins=int(mins)
-            print(mins,secs)
-            
-            
-            
+            mins=str(mins)
+            secs=str(secs)
+            duration_mins=mins+"."+secs
+            print(duration_mins)
+
         except Exception as e:
             pass
-           
+
+
